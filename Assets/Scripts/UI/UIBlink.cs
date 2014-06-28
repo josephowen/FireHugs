f933ede52887b659c85a766c9a00555a4b196c69
Darwin <darwinpek@gmail.com>
2014-06-28 15:11:44 -0700

    Added initial menu screen

---
+++ Assets/Scripts/UI/UIBlink.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIBlink : MonoBehaviour {
+	
+	public float m_time = 2;
+	private float m_elapsed = 0;
+
+	void Update () {
+		m_elapsed += Time.deltaTime;
+		while(m_elapsed > m_time) {
+			m_elapsed -= m_time;
+		}
+		renderer.material.color = new Color(1, 1, 1, Mathf.Abs(0.5f-(m_elapsed / m_time))*2);
+	}
+}
---
+++ Assets/Scripts/UI/UILogo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UILogo : MonoBehaviour {
+
+	public Texture m_texture;
+	public Vector2 m_targetPosition;
+
+	void Start () {
+		renderer.material.SetTexture(0, m_texture);
+		StartCoroutine(Move());
+	}
+
+	IEnumerator Move() {
+		float totalTime = 5;
+		float time = 0;
+		while(time < totalTime) {
+			yield return new WaitForEndOfFrame();
+			time += Time.deltaTime;
+			float ratio = time/totalTime;
+			this.transform.position = Vector3.Lerp(this.transform.position, m_targetPosition, Mathf.Sqrt(ratio));
+		}
+		this.transform.position = m_targetPosition;
+	}
+}
